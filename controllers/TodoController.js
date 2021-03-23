79efa40b966172cfe2cc9509785e4cf549b2fefd
Kamsi Kodi <kamsikodi@gmail.com>
2021-03-23 08:03:12 +0100

    Connected node to mongodb

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -1,25 +1,56 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{item: "get milk"}, {item: "walk dog"}];
+mongoose.connect('mongodb+srv://cluster0.9hlj4.mongodb.net', {
+    dbName: 'todo',
+    user: 'weird',
+    pass: 'test',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('connected'));
+
+//Create schema
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+// var itemOne = Todo({
+//     item: 'get-flowers'
+// }).save(function(err) {
+//     if (err) throw err;
+
+//     console.log('Item Saved');
+// });
+
+
+// var data = [{item: "get milk"}, {item: "walk dog"}];
 var urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function (app) {
     app.get('/todo', function (req, res) {
-        res.render('todo', {
-            todos: data
+        Todo.find({}, function (err, data) {
+            if (err) throw err;
+
+            res.render('todo', {
+                todos: data
+            });
         });
     });
 
     app.post('/todo', urlEncodedParser, function (req, res) {
-        data.push(req.body);
-        res.json(data);
+        var newTodo = Todo(req.body).save(function (err, data) {
+            if (err) throw err;
+            res.json(data);
+        });
     });
 
     app.delete('/todo/:item', function (req, res) {
-        data = data.filter(function (todo) {
-            return todo.item.replace(/ /g,'-') !== req.params.item; 
-        });
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).deleteOne(function(err, data) {
+            if (err) throw err;
+            res.json(data);
+        }); 
 
-        res.json(data);
+        
     });
 };
